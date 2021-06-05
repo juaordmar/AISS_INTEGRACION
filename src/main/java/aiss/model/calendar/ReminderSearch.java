c2c66affe0d999f7d59d3c65ec65335bf46125a5
juaordmar <juaordmar@alum.us.es>
2021-06-05 13:44:11 +0200

    ReminderSearch.java añadido

---
+++ src/main/java/aiss/model/calendar/ReminderSearch.java
@@ -0,0 +1,97 @@
+package aiss.model.calendar;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+	"id",
+	"date",
+	"type",
+	"title",
+	"description"
+})
+@Generated("jsonschema2pojo")
+public class ReminderSearch {
+
+	@JsonProperty("id")
+	private String id;
+	@JsonProperty("date")
+	private String date;
+	@JsonProperty("type")
+	private String type;
+	@JsonProperty("title")
+	private String title;
+	@JsonProperty("description")
+	private String description;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	@JsonProperty("id")
+	public String getId() {
+		return id;
+	}
+
+	@JsonProperty("id")
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@JsonProperty("date")
+	public String getDate() {
+		return date;
+	}
+
+	@JsonProperty("date")
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@JsonProperty("type")
+	public String getType() {
+		return type;
+	}
+
+	@JsonProperty("type")
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@JsonProperty("title")
+	public String getTitle() {
+		return title;
+	}
+
+	@JsonProperty("title")
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@JsonProperty("description")
+	public String getDescription() {
+		return description;
+	}
+
+	@JsonProperty("description")
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+}
