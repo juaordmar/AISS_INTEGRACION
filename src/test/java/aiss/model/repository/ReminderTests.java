a1a89f6264432c5a23e2fac245558c55c2f6669a
luicersan <80249145+luicersan@users.noreply.github.com>
2021-06-06 18:24:13 +0200

    Implementación del test del método PUT
    Algunos cambios menores en el formato de los prints

--- src/test/java/aiss/model/repository/ReminderTests.java
+++ src/test/java/aiss/model/repository/ReminderTests.java
@@ -19,24 +19,10 @@ public class ReminderTests {
 
 	static Reminder reminder, reminder2, reminder3, reminder4;
 	static ReminderResource reminderR = new ReminderResource();
-	static CalendarResource calendarR = new CalendarResource();
-<<<<<<< HEAD
 
-//	@BeforeClass
-//	public static void setUp() throws Exception{
-//
-//		Reminder rm1 = new Reminder();
-//		rm1.setTitle("Recordatorio test 1");
-//		rm1.setDate("21/04/2021");
-//		rm1.setDescription("descripción 1");
-//		rm1.setId("9");
-//		rm1.setType("calendario");
-//		reminder = reminderR.addReminder(rm1);
-//
-//	}
 	
 	@Test
-	public void createReminderTest() throws UnsupportedEncodingException {
+	public void addReminderTest() throws UnsupportedEncodingException {
 		String reminderId = "15"; //Cambiar ID y título en la defensa para no crear un Reminder igual
 		String reminderTitle = "Defensa de AISS proyecto integración";
 		
@@ -50,34 +36,15 @@ public class ReminderTests {
 		assertEquals("The reminder's id has not been set correctly", reminderId, reminder0.getId());
 		assertEquals("The reminder's title has not been set correctly", reminderTitle, reminder0.getTitle());
 		
-		System.out.println(reminder0.getId());
-		System.out.println(reminder0.getTitle());
+		System.out.println("ID of the reminder added: " + reminder0.getId());
+		System.out.println("Title of the reminder added: " + reminder0.getTitle());
 	}
 
-
-//	@Test
-//	public void getRemindersTest() throws UnsupportedEncodingException {
-//		//		ReminderResource reminderR = new ReminderResource();
-=======
-	
-	@BeforeClass
-	public static void setUp() throws Exception{
-		
-		Reminder rm1 = new Reminder();
-		rm1.setTitle("Recordatorio test 1");
-		rm1.setDate("21/04/2021");
-		rm1.setDescription("descripción 1");
-		rm1.setId("9");
-		rm1.setType("calendario");
-		reminder = reminderR.addReminder(rm1);
-		
-	}
 	
 	
 //	@Test
 //	public void getRemindersTest() throws UnsupportedEncodingException {
 ////		ReminderResource reminderR = new ReminderResource();
->>>>>>> 5f00567af1b0eca5020648650941d2d34b30a38e
 //		List<Reminder> reminders = reminderR.getReminders();
 //
 //		assertNotNull("The search returned null", reminders);
@@ -90,110 +57,40 @@ public class ReminderTests {
 //		}
 //
 //	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 5f00567af1b0eca5020648650941d2d34b30a38e
 //	@Test
 //	public void getReminderTest() throws UnsupportedEncodingException {
 //		ReminderResource reminderR = new ReminderResource();
-//		Reminder reminder = reminderR.getReminder("8");
+//		String reminderId = "8";
+//		Reminder reminder = reminderR.getReminder(reminderId);
 //
 //		assertNotNull("The search returned null", reminder);
-//		//assertFalse("The number of reminders is zero", reminders.size()==0);
 //
-//		//System.out.println("The search for reminders returned " + reminders.size() + " reminders.");
-//
-//		//for(Reminder reminder : reminders) {
-//		System.out.println("Reminder title: " + reminder.getTitle());
-//		//}
-<<<<<<< HEAD
+//			System.out.println("Reminder title with ID '" + reminderId + "' : "+ reminder.getTitle());
+//		}
+
 
 	}
 
 
-	//	@Test
-	//    public void testAddPlaylist() {
-	//        String playlistName = "Add playlist test title";
-	//        String playlistDescription = "Add playlist test description";
-	//
-	//        playlist4 = plr.addPlaylist(new Playlist(playlistName,playlistDescription));
-	//        reminder4 = reminderR.addReminder(new Reminder());
-	//
-	//        assertNotNull("Error when adding the playlist", playlist4);
-	//        assertEquals("The playlist's name has not been setted correctly", playlistName, playlist4.getName());
-	//        assertEquals("The playlist's description has not been setted correctly", playlistDescription, playlist4.getDescription());
-	//    }
-
 //	@Test
-//	public void testUpdateReminder() {
-//		String reminderTitle = "Updated reminder title";
+//	public void updateReminderTest() throws UnsupportedEncodingException { //Cambiar ID y título en la defensa para no crear un Reminder igual
+//		String reminderId = "3"; //Id del recordatorio a actualizar
+//		String reminderTitle = "Entrega ADDA";
 //
-//		// Update reminder
-//		reminder.setTitle(reminderTitle);
-//
-//		boolean success = reminderR.updateReminder(reminder);
+//		Reminder reminder0 = reminderR.getReminder(reminderId);
+//		reminder0.setTitle(reminderTitle);
+//		
+//		boolean success = reminderR.updateReminder(reminder0);
 //
 //		assertTrue("Error when updating the reminder", success);
-//
-//		Reminder r  = reminderR.getReminder(reminder.getId());
-//		assertEquals("The reminder's title has not been updated correctly", reminderTitle, r.getTitle());
+//		
+//		System.out.println("ID of the reminder updated: " + reminder0.getId());
+//		System.out.println("New title of the reminder updated: " + reminder0.getTitle());
+//	}
 //
 //	}
-	//
-	//    @Test
-	//    public void testDeletePlaylist() {
-	//        boolean success = plr.deletePlaylist(playlist2.getId());
-	//        assertTrue("Error when deleting the playlist", success);
-	//
-	//        Playlist pl = plr.getPlaylist(playlist2.getId());
-	//        assertNull("The playlist has not been deleted correctly", pl);
-	//    }
 
-=======
-//
-//	}
-	
-	
-//	@Test
-//    public void testAddPlaylist() {
-//        String playlistName = "Add playlist test title";
-//        String playlistDescription = "Add playlist test description";
-//
-//        playlist4 = plr.addPlaylist(new Playlist(playlistName,playlistDescription));
-//        reminder4 = reminderR.addReminder(new Reminder());
-//
-//        assertNotNull("Error when adding the playlist", playlist4);
-//        assertEquals("The playlist's name has not been setted correctly", playlistName, playlist4.getName());
-//        assertEquals("The playlist's description has not been setted correctly", playlistDescription, playlist4.getDescription());
-//    }
-
-    @Test
-    public void testUpdateReminder() {
-        String reminderTitle = "Updated reminder title";
-
-        // Update reminder
-        reminder.setTitle(reminderTitle);
-
-        boolean success = reminderR.updateReminder(reminder);
-
-        assertTrue("Error when updating the reminder", success);
-
-        Reminder r  = reminderR.getReminder(reminder.getId());
-        assertEquals("The reminder's title has not been updated correctly", reminderTitle, r.getTitle());
-
-    }
-//
-//    @Test
-//    public void testDeletePlaylist() {
-//        boolean success = plr.deletePlaylist(playlist2.getId());
-//        assertTrue("Error when deleting the playlist", success);
-//
-//        Playlist pl = plr.getPlaylist(playlist2.getId());
-//        assertNull("The playlist has not been deleted correctly", pl);
-//    }
->>>>>>> 5f00567af1b0eca5020648650941d2d34b30a38e
 
 
 
